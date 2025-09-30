45deb028313960f584936cce80e83abeaf6b34a9
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-09-30 10:12:53 -0400

    Create logicOpparatorsComments.js

---
+++ logicOpparatorsComments.js
@@ -0,0 +1,9 @@
+// logical AND : &&
+// logical OR : ||
+// logical NOT : !
+// logical NCO : ??
+
+// AND => and: condition and condition met => true, else => false
+// OR => or: condition or condition met => true, else => false
+// NOT => not/is not: input is not condition => true, input is condition => false
+// NCO => Nullish Coalescing Operator: left hand opp is null => right hand opp, else => left hand opp
